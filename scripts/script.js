329b0ace21564a1deadb680d92aa39526106349e
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-02-16 17:52:46 -0300

    feat: exibe na tela as lista de quizzes criados pelo usuário e a lista de quizzes criados por outros usuários de maneira separadamente na tela

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,20 @@
 const userQuizzesHtmlClass = document.querySelector(".home_created-quizzes");
-const generalQuizzesHtmlCLass = document.querySelector(".home_general-quizzes");
-// const quizzesCreatedByUser = JSON.parse(localStorage.getItem("userQuizz"));
-
-// function getUserQuizzes(){
-
-// }
+const nonUserQuizzesHtmlCLass = document.querySelector(".home_non-user-quizzes");
+// const storedUserQuizzes = JSON.parse(localStorage.getItem("userQuizz"));
+const storedUserQuizzes = [{ id: -1 }, { id: -2 }, { id: -3 }]; //for testing only
 
 const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes")
-promise.then(printGeneralQuizzes)
+promise.then(printQuizzes);
 
-function printGeneralQuizzes(response) {
-    response.data.forEach(element => {
-        console.log(element)
-        generalQuizzesHtmlCLass.innerHTML += `
-        
+function printQuizzes(response) {
+    // printUserQuizzes();
+    printNonUserQuizzes(response);
+}
+
+function printUserQuizzes() {
+    storedUserQuizzes.forEach(element => {
+        userQuizzesHtmlClass += `
+
         <article class="home_quizz" onclick="openQuizzView()">
             <img src="${element.image}" alt="">
             <div class="home_quizz_gradient"></div>
@@ -21,8 +22,42 @@ function printGeneralQuizzes(response) {
         </article>
         `
     });
-    console.log(response);
 }
+
+function printNonUserQuizzes(response) {
+    let nonUserQuizzesList = response.data.filter(filterUserQuizzes);
+    // console.log(nonUserQuizzesList)
+    nonUserQuizzesList.forEach(element => {
+        nonUserQuizzesHtmlCLass.innerHTML += `
+
+        <article class="home_quizz" onclick = "openQuizzView()" >
+            <img src="${element.image}" alt="">
+            <div class="home_quizz_gradient"></div>
+            <p>${element.title}</p>
+        </article>
+        `
+    });
+}
+
+function filterUserQuizzes(generalQuizz) {
+    // if (1 === 1) {
+    //     return true;
+    // }
+
+    let comparingArray = [];
+    storedUserQuizzes.forEach(userQuizz => {
+        // console.log("user quizz id: " + userQuizz.id)
+        if (generalQuizz.id !== userQuizz.id) {
+            comparingArray.push(true);
+        }
+        console.log(comparingArray)
+    });
+    
+    if(){
+        
+    }
+}
+
 
 function openQuizzView() {
     document.querySelector(".home").classList.toggle("hidden");
